fa6b802db57ddfc0fc9be5eef1a823ea79eca610
AhmedEssam17 <ahmedessam020@gmail.com>
2025-02-15 02:09:36 +0200

    Problem 02

---
+++ problem_02/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"time"
+)
+
+/*
+	===========================================================================================
+	Problem 02
+	===========================================================================================
+	Write the Linux command needed to change a file name from original.txt to changed.txt
+*/
+
+/*
+	===========================================================================================
+	Proposed Solution
+	===========================================================================================
+	Procedure:
+		So the linux command to rename a file called original.txt to changed.txt is as follows:
+			"mv original.txt changed.txt"
+	Here is a simple go code that runs as follows:
+		1) Delete changed.txt if exists from previous runs.
+		2) Create the "original.txt" file,
+		3) Wait 3 seconds to see the file clearly in the file explorer.
+		4) Proceed to run the "mv" linux command to rename it to changed.txt
+*/
+
+func main() {
+	oldName := "original.txt"
+	newName := "changed.txt"
+
+	// Delete changed.txt if exists from previous runs.
+	cmd := exec.Command("rm", "-f", newName)
+	_ = cmd.Run()
+
+	// Create the "original.txt" file
+	cmd = exec.Command("touch", oldName)
+	_ = cmd.Run()
+
+	// Wait 3 seconds to see the file clearly in the file explorer.
+	time.Sleep(3 * time.Second)
+
+	// Run the "mv" linux command to rename it to changed.txt
+	cmd = exec.Command("mv", oldName, newName)
+	_ = cmd.Run()
+
+	fmt.Println("File renamed successfully using Linux command!")
+}
